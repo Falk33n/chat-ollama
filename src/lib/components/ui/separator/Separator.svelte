f0fa8f85de44b85914373dac9ec3ede1cf9afeba
Falk33n <tim.falk00@gmail.com>
2025-03-09 16:05:01 +0100

    feat: add separator

---
+++ src/lib/components/ui/separator/Separator.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { SeparatorProps } from '$components/ui/separator';
+	import { cn } from '$utils';
+	import { Separator as SeparatorPrimitive } from 'bits-ui';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		orientation = 'horizontal',
+		...restProps
+	}: SeparatorProps = $props();
+</script>
+
+<SeparatorPrimitive.Root
+	bind:ref
+	class={cn(
+		'bg-border shrink-0',
+		orientation === 'horizontal' ? 'h-[1px] w-full' : 'min-h-full w-[1px]',
+		className,
+	)}
+	{orientation}
+	{...restProps}
+/>
