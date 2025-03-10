636bbc0c558d9dddee44381726cb4c3809c0cabd
Falk33n <tim.falk00@gmail.com>
2025-03-10 20:15:00 +0100

    fix: revert commit "fix: git error"
    This reverts commit bea47d7841c06022ca3c4a8f46e94c633a175a94.

---
+++ src/lib/components/global/index.ts
@@ -0,0 +1,1 @@
+export { default as AppTooltip } from '$components/global/AppTooltip.svelte';
---
+++ src/lib/components/layout/AppSidebarHeader.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { AppTooltip } from '$components/global';
+	import { Button } from '$components/ui/button';
+	import { SidebarHeader, useSidebar } from '$components/ui/sidebar';
+	import { PanelLeftCloseIcon, SearchIcon, SquarePenIcon } from 'lucide-svelte';
+
+	const sidebar = useSidebar();
+</script>
+
+<SidebarHeader class="mb-5 flex-row items-center justify-between gap-2">
+	<AppTooltip content="Close Sidebar">
+		{#snippet trigger({ props: { onclick: _onclick, ...props } })}
+			<Button
+				variant="outline"
+				size="icon"
+				aria-label="Close Sidebar"
+				data-sidebar="trigger"
+				onclick={() => {
+					sidebar.toggle();
+				}}
+				{...props}
+			>
+				<PanelLeftCloseIcon aria-hidden />
+			</Button>
+		{/snippet}
+	</AppTooltip>
+
+	<div class="flex items-center justify-center gap-2">
+		<AppTooltip content="Search in chats">
+			{#snippet trigger({ props: { onclick: _onclick, ...props } })}
+				<Button
+					variant="outline"
+					size="icon"
+					aria-label="Search in chats"
+					{...props}
+				>
+					<SearchIcon aria-hidden />
+				</Button>
+			{/snippet}
+		</AppTooltip>
+
+		<AppTooltip content="New chat">
+			{#snippet trigger({ props: { onclick: _onclick, ...props } })}
+				<Button
+					variant="outline"
+					size="icon"
+					aria-label="New chat"
+					{...props}
+				>
+					<SquarePenIcon aria-hidden />
+				</Button>
+			{/snippet}
+		</AppTooltip>
+	</div>
+</SidebarHeader>
