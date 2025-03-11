871e3901997800fe1f58f9931cca1102d2f16643
Falk33n <tim.falk00@gmail.com>
2025-03-11 19:52:27 +0100

    feat: make infinite scroll work, and add loading icon on scroll

--- src/lib/components/layout/AppSidebarTrigger.svelte
+++ src/lib/components/layout/AppSidebarTrigger.svelte
@@ -1,19 +1,15 @@
 <script lang="ts">
 	import { AppTooltip } from '$components/global';
-	import { SidebarTrigger, useSidebar } from '$components/ui/sidebar';
-
-	const sidebar = useSidebar();
+	import { SidebarTrigger } from '$components/ui/sidebar';
 </script>
 
-{#if !sidebar.open || sidebar.isMobile}
-	<div class="top-2 left-2 fixed">
-		<AppTooltip content="Open Sidebar">
-			{#snippet trigger({ props: { onclick: _onclick, ...props } })}
-				<SidebarTrigger
-					aria-label="Open Sidebar"
-					{...props}
-				/>
-			{/snippet}
-		</AppTooltip>
-	</div>
-{/if}
+<div class="fixed top-2 left-2">
+	<AppTooltip content="Open Sidebar">
+		{#snippet trigger({ props: { onclick: _onclick, ...props } })}
+			<SidebarTrigger
+				aria-label="Open Sidebar"
+				{...props}
+			/>
+		{/snippet}
+	</AppTooltip>
+</div>
---
+++ src/lib/components/layout/NewChatTrigger.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { AppTooltip } from '$components/global';
+	import { Button } from '$components/ui/button';
+	import { SquarePenIcon } from 'lucide-svelte';
+</script>
+
+<AppTooltip content="New chat">
+	{#snippet trigger({ props: { onclick: _onclick, ...props } })}
+		<Button
+			variant="outline"
+			size="icon"
+			class="bg-sidebar hover:bg-background"
+			aria-label="New chat"
+			{...props}
+		>
+			<SquarePenIcon aria-hidden />
+		</Button>
+	{/snippet}
+</AppTooltip>
