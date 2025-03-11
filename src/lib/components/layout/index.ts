2d2f1d415e01333219c1deaa399d4be1a07f53a5
Falk33n <tim.falk00@gmail.com>
2025-03-11 12:50:40 +0100

    feat: add import for context

--- src/lib/components/layout/index.ts
+++ src/lib/components/layout/index.ts
@@ -9,6 +9,11 @@ export { default as AppSidebarHeader } from '$components/layout/AppSidebarHeader
 
 export { default as ProfileSettingsTrigger } from '$components/layout/ProfileSettingsTrigger.svelte';
 
-export { default as AppSidebarMenuItem } from '$components/layout/AppSidebarMenuItem.svelte';
+export { default as ChatSettingsTrigger } from '$components/layout/ChatSettingsTrigger.svelte';
+
+export {
+	default as AppSidebarMenuItem,
+	getAppSidebarMenuItemContext,
+} from '$components/layout/AppSidebarMenuItem.svelte';
 
 export { default as ModeToggle } from '$components/layout/ModeToggle.svelte';
