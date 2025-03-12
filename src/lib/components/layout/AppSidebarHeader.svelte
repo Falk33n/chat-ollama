793df53e0a4ebcb930c24606cdeb96bbbd1f31be
Falk33n <tim.falk00@gmail.com>
2025-03-12 11:47:15 +0100

    feat: use app sidebar trigger in the header instead of a button

--- src/lib/components/layout/AppSidebarHeader.svelte
+++ src/lib/components/layout/AppSidebarHeader.svelte
@@ -1,31 +1,15 @@
 <script lang="ts">
 	import { AppTooltip } from '$components/global';
-	import { NewChatTrigger } from '$components/layout';
+	import { AppSidebarTrigger, NewChatTrigger } from '$components/layout';
 	import { Button } from '$components/ui/button';
-	import { SidebarHeader, useSidebar } from '$components/ui/sidebar';
+	import { SidebarHeader } from '$components/ui/sidebar';
 	import { PanelLeftCloseIcon, SearchIcon } from 'lucide-svelte';
-
-	const sidebar = useSidebar();
 </script>
 
 <SidebarHeader class="mb-4 flex-row items-center justify-between gap-2">
-	<AppTooltip content="Close Sidebar">
-		{#snippet trigger({ props: { onclick: _onclick, ...props } })}
-			<Button
-				variant="outline"
-				size="icon"
-				aria-label="Close Sidebar"
-				class="bg-sidebar hover:bg-background"
-				data-sidebar="trigger"
-				onclick={() => {
-					sidebar.toggle();
-				}}
-				{...props}
-			>
-				<PanelLeftCloseIcon aria-hidden />
-			</Button>
-		{/snippet}
-	</AppTooltip>
+	<AppSidebarTrigger class="bg-sidebar hover:bg-background">
+		<PanelLeftCloseIcon aria-hidden />
+	</AppSidebarTrigger>
 
 	<div class="flex items-center justify-center gap-2">
 		<AppTooltip content="Search in chats">
@@ -42,6 +26,6 @@
 			{/snippet}
 		</AppTooltip>
 
-		<NewChatTrigger />
+		<NewChatTrigger class="bg-sidebar hover:bg-background" />
 	</div>
 </SidebarHeader>
