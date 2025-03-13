bdaa0b41bd089bef90c2cd5bd6fca4ee81ca437c
Falk33n <tim.falk00@gmail.com>
2025-03-13 15:51:52 +0100

    feat: add dialog

---
+++ src/lib/components/ui/dialog/DialogDescription.svelte
@@ -0,0 +1,27 @@
+<script
+	lang="ts"
+	module
+>
+	import { Dialog as DialogPrimitive, type WithElementRef } from 'bits-ui';
+
+	export type DialogDescriptionProps = WithElementRef<
+		Omit<DialogPrimitive.DescriptionProps, 'ref'>,
+		HTMLDivElement
+	>;
+</script>
+
+<script lang="ts">
+	import { cn } from '$utils';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		...restProps
+	}: DialogDescriptionProps = $props();
+</script>
+
+<DialogPrimitive.Description
+	bind:ref
+	class={cn('text-muted-foreground text-sm', className)}
+	{...restProps}
+/>
